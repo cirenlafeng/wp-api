6aa5db1f09c31612cb369c381c9996c2119bdc00
caishiqiang <sean.cai@onemena.com>
2018-03-27 12:33:20 +0800

    test

--- base.php
+++ base.php
@@ -8,7 +8,7 @@ $dbName=DB_NAME;    //使用的数据库
 $user=DB_USER;      //数据库连接用户名
 $pass=DB_PASSWORD;  //对应的密码
 $dsn="$dbms:host=$host;dbname=$dbName";
-echo DB_HOST;
+echo dirname(__DIR__).'\wp-config.php';
 exit();
 
 try {
